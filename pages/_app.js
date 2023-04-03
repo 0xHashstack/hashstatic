cd3cd1e3de973ada4a68397361faa83fddb10824
VenkatTeja <teja.kunisetty@gmail.com>
2023-04-03 15:42:11 +0530

    add twitter tracking

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Fonts from "../theme/Fonts";
 import { theme } from "../theme/theme";
 import "../styles/globals.css";
 import { useEffect } from "react";
+import Script from 'next/script'
 
 //Import clarity
 import { clarity } from "react-microsoft-clarity";
@@ -69,8 +70,20 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:type" content="website" />
         <meta content="summary_large_image" name="twitter:card" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+      
       </Head>
       <ChakraProvider theme={theme}>
+
+        {/* <!-- Twitter conversion tracking base code --> */}
+        <Script id="twitter-tracking" strategy="afterInteractive">
+          {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','oegr3');
+            `}
+        </Script>
+        {/* <!-- End Twitter conversion tracking base code --> */}
+
         <Fonts />
         <Component {...pageProps} />
       </ChakraProvider>
