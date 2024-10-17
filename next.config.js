9fee916a14ba4b310f08d31c290f6e58684119b5
Sahitya77 <sahityanijhawan@gmail.com>
2024-10-17 18:18:51 +0530

    REFACTORING added safety implementation for config [DONE]

--- next.config.js
+++ next.config.js
@@ -1,11 +1,40 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
-  }
+  },
+  swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              "camera=(); battery=(self); geolocation=(); microphone=('https://a-domain.com')",
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
